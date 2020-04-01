95d34ed93caa63afdd70c236eebee940dfeebfcc
maggiflo98 <maggiflo98@gmail.com>
2020-04-01 12:09:06 +0300

    fixed bug,implemented log in with facebook

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const Path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook');
 const hbs = require('express-handlebars');
 
 
 const app = express();
+
+app.use (bodyParser.json());
+
+app.use (bodyParser.urlencoded({extended:false}));
+
 // signup route
 const usersRoute= require('./routes/users');
 
 app.use('/users', usersRoute);
-// app.use('/login',loginRoutes);
+
+var FACEBOOK_APP_ID = "210104406973848",
+    FACEBOOK_APP_SECRET="2a709187e2f19fd80176b8caf6f767dc";
+
+   // var fbOpts = {
+   //    clientId:FACEBOOK_APP_ID,
+   //    clientSecret:FACEBOOK_APP_SECRET,
+   //    callbackUrl:'http://localhost:8005/auth/facebook/callback'
+   // };
+   
+   var fbCallback = function(accessToken,refreshToken,profile,callback){
+      
+
+   };
+
+// passport.use(new FacebookStrategy(fbCallback,({
+//    clientId:FACEBOOK_APP_ID,
+//        clientSecret:FACEBOOK_APP_SECRET,
+//        callbackUrl:'http://localhost:8005/auth/facebook/callback'
+
+// })));
+
+passport.use(new FacebookStrategy({
+   clientID: FACEBOOK_APP_ID,
+   clientSecret: FACEBOOK_APP_SECRET,
+   callbackUrl:'http://localhost:8005/auth/facebook/callback'
+
+ }, function(accessToken, refreshToken, profile, done) {
+   User.findOrCreate({facebookId: profile.id}, function (error, user) {
+     return done(error, user);
+   });
+ }
+));
 
 
 app.set('view engine', 'hbs');//instead of app.engine('handlebars', handlebars({
@@ -24,23 +65,36 @@ app.engine('hbs', hbs({layoutsDir: __dirname + '/views/layouts', extname: 'hbs'}
 // app.use(express.static(__dirname + '/public'));
 
 
-app.use (bodyParser.json());
+// app.use (bodyParser.json());
 
 
-app.use (bodyParser.urlencoded({extended:true}));
+// app.use (bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
    res.render('index')
 });
 
+// app.route('/')
+//    .get(passport.authenticate('facebook'));
+
+app.route('/auth/facebook/callback')
+   .get(passport.authenticate('facebook')
+      //  res.send('this checks the status of the request.Good.luck')
+   );
+
 app.get('/signup',function(req,res){
    res.render('signup')
 });
 
+app.get('/login',function(req,res){
+   res.render('login')
+
+});
 
 
-const port=8003;
+
+const port=8005;
 
 app.listen(port,function(){
    console.log("listening on "+ port)
--- routes/users.js
+++ routes/users.js
@@ -10,16 +10,17 @@ router.get('/signup',function (req,res) {
      res.render('signup');
 });
 router.post('/signup',function (req,res) {
-    var username= req.body.username;
+    var username = req.body.username;
     var email = req.body.email;
     var password1 = req.body.password1;
     var password2 = req.body.password2;
 
 
-
+    const saltrounds =10;
     if(password1==password2) {
-        bcryptjs.hash("password1, 10",  function ( err,hash)
-        {
+        bcryptjs.hash(password1, 10,  function ( err,hash)
+        { 
+            password1=hash;
              console.log(hash);
 
             var sql = "INSERT INTO `usersdb`(`id`, `username`, `email`, `password`) VALUES (NULL,'" +username+ "','" + email + "','"+hash+"')";
@@ -33,6 +34,33 @@ router.post('/signup',function (req,res) {
             });
         });
     }
+
+
+ });
+
+ router.get('/login',function(req,res){
+    res.render('login');
+ });
+
+ router.post('/login',function(req,res){
+   var email=req.body.email;
+   var password =req.body.password;
+
+// check   if user exists 
+if (email == email && password==password){
+var sql="SELECT * FROM `usersdb` WHERE email = '"+email+"' AND password = '"+password+"'";
+db.query(sql,function(err,result){
+    if(err){
+    throw err;
+    }else{
+        res.send('logged in successfuly')
+    }
+
+});
+
+
+}
+ 
 });
 
 
